9d31d99cb917919a74541981c1eb363beaf0de59
Mike Martinez <mike.j.martinez99@gmail.com>
2025-06-24 12:12:54 -0400

    Add support for SE and PE

--- make_pe_sample_sheet.sh
+++ make_pe_sample_sheet.sh
@@ -5,25 +5,38 @@
 #----- Set directory containing fastq.gz files
 FASTQ_DIR="."  # or change to your directory, e.g., ./fastqs
 OUTPUT_CSV="samples.csv"
+LAYOUT="paired"
 
-#----- Header
-echo "fastq_1,fastq_2" > "$OUTPUT_CSV"
+if [[ "$LAYOUT" == "paired" ]]; then
+	#----- Generate header
+	echo "fastq_1,fastq_2" > "$OUTPUT_CSV"
+	
+	#----- Loop over all R1 files and find matching R2
+	for r1 in "$FASTQ_DIR"/*_R1*.fastq.gz; do
+		#----- Skip if not match 
+		[ -e "$r1" ] || continue
+		
+		#----- Derive R2 filename with sed
+		r2="${r1/_R1/_R2}"	
+		
+		#----- Check that the R2 file exists
+		if [[ -f "$r2" ]]; then
+			echo "$r1,$r2" >> "$OUTPUT_CSV"
+		else
+			echo "Warning, No R2 file found for $r1" >&2
+		fi
+	done
+else
+	#----- Generate header
+	echo "fastq_1" > "$OUTPUT_CSV"
+	
+	for f in "$FASTQ_DIR"/*.fastq.gz; do
+  		[ -e "$f" ] && echo "$f" >> "$OUTPUT_CSV"
+	done
+fi
+	
 
-#----- Loop over all R1 files and find matching R2
-for r1 in "$FASTQ_DIR"/*_R1*.fastq.gz; do
-    #----- Skip if no match (in case glob fails)
-    [ -e "$r1" ] || continue
 
-    #----- Derive R2 filename
-    r2="${r1/_R1/_R2}"
-
-    #----- Check if R2 file exists
-    if [[ -f "$r2" ]]; then
-        echo "$r1,$r2" >> "$OUTPUT_CSV"
-    else
-        echo "Warning: No R2 found for $r1" >&2
-    fi
-done
 
 #----- Echo statement
 echo "CSV written to $OUTPUT_CSV"
